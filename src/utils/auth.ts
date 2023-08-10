693483079c4c3f35e55d2779a3953e8dc6ae5e76
darikmohammed <r.m.darik@gmail.com>
2023-08-10 22:31:47 +0300

    Update auth for Bearer token

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,14 @@
 import jwt from 'jsonwebtoken'
 import { GraphQLError } from 'graphql'
 
-const getUserIdFromToken = (token: string) => {  
+const getUserIdFromToken = (totalToken: string) => {
+
+  if(totalToken.split(' ')[0] !== 'Bearer') throw new GraphQLError('You are not authorized to perform this action.', {
+    extensions: {
+      code: 'FORBIDDEN',
+    },
+  });
+  const token =  totalToken.split(' ')[1]
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY as string) as {userId: string, requireTwoFactor: boolean}    
     return {userId: decoded.userId, requireTwoFactor: decoded.requireTwoFactor}
